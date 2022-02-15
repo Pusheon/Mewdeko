8912ef479e31db7fc5ff0d3baf9771669a3cdef6
YunoGasai67 <59923820+YunoGasai67@users.noreply.github.com>
2022-02-14 19:22:25 -0500

    remove invite references

--- src/Mewdeko/_Extensions/Extensions.cs
+++ src/Mewdeko/_Extensions/Extensions.cs
@@ -55,39 +55,31 @@ public static class Extensions
 
 
     public static async Task SendConfirmAsync(this IDiscordInteraction interaction, string message)
-        => await interaction.RespondAsync(embed: new EmbedBuilder().WithOkColor().WithDescription(message).Build(), components: new ComponentBuilder()
-            .WithButton(label:"Invite Me!", style: ButtonStyle.Link, url: "https://top.gg/bot/752236274261426212").Build());
+        => await interaction.RespondAsync(embed: new EmbedBuilder().WithOkColor().WithDescription(message).Build());
     
     public static async Task SendEphemeralConfirmAsync(this IDiscordInteraction interaction, string message) 
-        => await interaction.RespondAsync(embed: new EmbedBuilder().WithOkColor().WithDescription(message).Build(), ephemeral: true, components: new ComponentBuilder()
-            .WithButton(label:"Invite Me!", style: ButtonStyle.Link, url: "https://top.gg/bot/752236274261426212").Build());
+        => await interaction.RespondAsync(embed: new EmbedBuilder().WithOkColor().WithDescription(message).Build());
     
     public static async Task SendErrorAsync(this IDiscordInteraction interaction, string message) 
-        => await interaction.RespondAsync(embed: new EmbedBuilder().WithErrorColor().WithDescription(message).Build(), components: new ComponentBuilder()
-            .WithButton(label: "Support Server", style: ButtonStyle.Link, url: "https://discord.gg/wB9FBMreRk").Build());
+        => await interaction.RespondAsync(embed: new EmbedBuilder().WithErrorColor().WithDescription(message).Build());
     
     public static async Task SendEphemeralErrorAsync(this IDiscordInteraction interaction, string message) 
-        => await interaction.RespondAsync(embed: new EmbedBuilder().WithErrorColor().WithDescription(message).Build(), ephemeral: true, components: new ComponentBuilder()
-            .WithButton(label: "Support Server", style: ButtonStyle.Link, url: "https://discord.gg/wB9FBMreRk").Build());
+        => await interaction.RespondAsync(embed: new EmbedBuilder().WithErrorColor().WithDescription(message).Build(), ephemeral: true);
     
     public static async Task<IUserMessage> SendConfirmFollowupAsync(this IDiscordInteraction interaction, string message) 
-        => await interaction.FollowupAsync(embed: new EmbedBuilder().WithOkColor().WithDescription(message).Build(), components: new ComponentBuilder()
-            .WithButton(label:"Invite Me!", style: ButtonStyle.Link, url: "https://top.gg/bot/752236274261426212").Build());
+        => await interaction.FollowupAsync(embed: new EmbedBuilder().WithOkColor().WithDescription(message).Build());
 
     public static async Task<IUserMessage> SendConfirmFollowupAsync(this IDiscordInteraction interaction, string message, ComponentBuilder builder) 
-        => await interaction.FollowupAsync(embed: new EmbedBuilder().WithOkColor().WithDescription(message).Build(), components: builder.Build());
+        => await interaction.FollowupAsync(embed: new EmbedBuilder().WithOkColor().WithDescription(message).Build());
     
     public static async Task<IUserMessage> SendEphemeralFollowupConfirmAsync(this IDiscordInteraction interaction, string message)
-        => await interaction.FollowupAsync(embed: new EmbedBuilder().WithOkColor().WithDescription(message).Build(), ephemeral: true, components: new ComponentBuilder()
-            .WithButton(label:"Invite Me!", style: ButtonStyle.Link, url: "https://top.gg/bot/752236274261426212").Build());
+        => await interaction.FollowupAsync(embed: new EmbedBuilder().WithOkColor().WithDescription(message).Build());
     
     public static async Task<IUserMessage> SendErrorFollowupAsync(this IDiscordInteraction interaction, string message) 
-        => await interaction.FollowupAsync(embed: new EmbedBuilder().WithErrorColor().WithDescription(message).Build(), components: new ComponentBuilder()
-            .WithButton(label: "Support Server", style: ButtonStyle.Link, url: "https://discord.gg/wB9FBMreRk").Build());
+        => await interaction.FollowupAsync(embed: new EmbedBuilder().WithErrorColor().WithDescription(message).Build());
     
     public static async Task<IUserMessage> SendEphemeralFollowupErrorAsync(this IDiscordInteraction interaction, string message) 
-        => await interaction.FollowupAsync(embed: new EmbedBuilder().WithErrorColor().WithDescription(message).Build(), ephemeral: true, components: new ComponentBuilder()
-            .WithButton(label: "Support Server", style: ButtonStyle.Link, url: "https://discord.gg/wB9FBMreRk").Build());
+        => await interaction.FollowupAsync(embed: new EmbedBuilder().WithErrorColor().WithDescription(message).Build(), ephemeral: true);
     
     
     public static bool IsValidAttachment(this IReadOnlyCollection<IAttachment> attachments)
--- src/Mewdeko/_Extensions/IMessageChannelExtensions.cs
+++ src/Mewdeko/_Extensions/IMessageChannelExtensions.cs
@@ -22,8 +22,7 @@ public static class MessageChannelExtensions
         return ch.SendMessageAsync("", embed: eb.Build());
     }
     
-    public static Task<IUserMessage> SendErrorAsync(this IMessageChannel ch, string error) => ch.SendMessageAsync("", embed: new EmbedBuilder().WithErrorColor().WithDescription(error).Build(), components: new ComponentBuilder()
-        .WithButton(label: "Support Server", style: ButtonStyle.Link, url: "https://discord.gg/wB9FBMreRk").Build());
+    public static Task<IUserMessage> SendErrorAsync(this IMessageChannel ch, string error) => ch.SendMessageAsync("", embed: new EmbedBuilder().WithErrorColor().WithDescription(error).Build());
     public static Task<IUserMessage> SendConfirmAsync(this IMessageChannel ch, string title, string text,
         string url = null, string footer = null)
     {
@@ -36,8 +35,7 @@ public static class MessageChannelExtensions
         return ch.SendMessageAsync("", embed: eb.Build());
     }
     public static Task<IUserMessage> SendConfirmAsync(this IMessageChannel ch, string text) =>
-        ch.SendMessageAsync(embed: new EmbedBuilder().WithOkColor().WithDescription(text).Build(), components: new ComponentBuilder()
-            .WithButton(label:"Invite Me!", style: ButtonStyle.Link, url: "https://top.gg/bot/752236274261426212").Build());
+        ch.SendMessageAsync(embed: new EmbedBuilder().WithOkColor().WithDescription(text).Build());
     
 
     public static Task<IUserMessage> SendConfirmAsync(this ITextChannel ch, string text,
